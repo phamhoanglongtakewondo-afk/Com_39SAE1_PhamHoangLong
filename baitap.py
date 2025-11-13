ffbd4e595ab1329cc7ecd5422191aa5f5967a763
phamhoanglongtakewondo-afk <phamhoanglongtakewondo@gmail.com>
2025-11-13 15:20:00 +0700

    ok

--- baitap.py
+++ baitap.py
@@ -1,3 +1,10 @@
-tuoi, ten, PI = 17,"Long", 3.14
-print(tuoi,ten,PI)
-print(type(tuoi),type(ten),type(PI))
+#W2A4
+a1,b1,c1,a2,b2,a3 = map(float, input().split())
+A = ((a1 + b1 + c1) + (a2 + b2)*2 +(a3)*3) / 10
+print("average: ", round(A))
+#W2A7
+A = ((13 ** 2) * 3) + 5
+B = 13**2*3 + 5
+print(A, B)
+
+
